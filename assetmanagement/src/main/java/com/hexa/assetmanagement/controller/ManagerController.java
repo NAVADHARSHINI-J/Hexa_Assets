93fe5068080e1a23393acb55dcc386a1484ca4fb
Navadharshini.J <navadharshinij@gmail.com>
2025-04-05 10:28:42 +0530

    user added in manager

--- assetmanagement/src/main/java/com/hexa/assetmanagement/controller/ManagerController.java
+++ assetmanagement/src/main/java/com/hexa/assetmanagement/controller/ManagerController.java
@@ -1,11 +1,13 @@
 package com.hexa.assetmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hexa.assetmanagement.exception.InvalidIdException;
 import com.hexa.assetmanagement.model.Manager;
 import com.hexa.assetmanagement.service.ManagerService;
 
@@ -16,8 +18,18 @@ public class ManagerController {
 	@Autowired
 	private ManagerService managerService;
 	
-	@PostMapping("/add")
-	public Manager add(@RequestBody Manager manager) {
-		return managerService.add(manager);
+	@PostMapping("/add/{id}")
+	public Manager add(@RequestBody Manager manager,@PathVariable int id) throws InvalidIdException {
+		return managerService.add(manager,id);
 	}
 }
+
+
+
+
+
+
+
+
+
+
