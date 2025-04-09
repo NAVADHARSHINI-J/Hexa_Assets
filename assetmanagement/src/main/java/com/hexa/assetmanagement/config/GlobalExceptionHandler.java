885af85ee04bb0042003ceb6a2da8d0b60a936a0
Navadharshini.J <navadharshinij@gmail.com>
2025-04-09 19:16:24 +0530

    added api

--- assetmanagement/src/main/java/com/hexa/assetmanagement/config/GlobalExceptionHandler.java
+++ assetmanagement/src/main/java/com/hexa/assetmanagement/config/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.hexa.assetmanagement.exception.AssetUnavailableException;
 import com.hexa.assetmanagement.exception.InvalidContactException;
 import com.hexa.assetmanagement.exception.InvalidIdException;
 import com.hexa.assetmanagement.exception.UsernameInvalidException;
@@ -29,4 +30,9 @@ public class GlobalExceptionHandler {
 	public ErrorResponse ErrorHandler(Exception e) {
 		return ErrorResponse.create(e, HttpStatusCode.valueOf(400), e.getMessage());
 	}
+	@ExceptionHandler(AssetUnavailableException.class)
+	public ErrorResponse AssetUnavailableExceptionHandler(AssetUnavailableException e) {
+		return ErrorResponse.create(e, HttpStatusCode.valueOf(400), e.getMessage());
+	}
+	
 }
--- assetmanagement/src/main/java/com/hexa/assetmanagement/repository/AssetAllocationRepository.java
+++ assetmanagement/src/main/java/com/hexa/assetmanagement/repository/AssetAllocationRepository.java
@@ -2,8 +2,10 @@ package com.hexa.assetmanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 import com.hexa.assetmanagement.model.AssetAllocation;
 
 public interface AssetAllocationRepository extends JpaRepository<AssetAllocation, Integer> {
+
 
 }
