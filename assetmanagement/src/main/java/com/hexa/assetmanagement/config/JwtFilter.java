6c5c54977811028052dca76d1018978dfda9d974
Sheryl-AC <acsheryl029@gmail.com>
2025-05-05 09:35:19 +0530

    changes done

--- assetmanagement/src/main/java/com/hexa/assetmanagement/config/JwtFilter.java
+++ assetmanagement/src/main/java/com/hexa/assetmanagement/config/JwtFilter.java
@@ -40,21 +40,10 @@ public class JwtFilter extends OncePerRequestFilter{
 		 * using userSecurityService, i will fetch user details by username
 		 * role..
 		 * */
-<<<<<<< HEAD
-<<<<<<< HEAD
-         final String authorizationHeader = request.getHeader("Authorization");
-		//final String authorizationHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJTaGVyeWwiLCJleHAiOjE3NDU5NDY2NTAsImlhdCI6MTc0NTg2MDI1MH0.8F9zm3IkEbSUqBZwZVf7lwSY9SkJJahHRCbzsiFFLgc";
+         //final String authorizationHeader = request.getHeader("Authorization");
+		final String authorizationHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJNYXJ5IiwiZXhwIjoxNzQ2NDkyOTg5LCJpYXQiOjE3NDY0MDY1ODl9.gqjUXY4Fpszd2gAJyubBZ02GPrJ6yrvGg0ifNhX67nk";
 		System.out.println("authorization header"+authorizationHeader);
-=======
 //		final String authorizationHeader = request.getHeader("Authorization");
-		final String authorizationHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbGllbiIsImV4cCI6MTc0NjM0NDExOSwiaWF0IjoxNzQ2MjU3NzE5fQ.L2Ms8EAx3FTpSH-oi3yu8wDD5qo1Orv7NszyYxt4OBY";
-		
->>>>>>> 352c588786706697ddb2acd89f08faa5afdca60a
-=======
-//		final String authorizationHeader = request.getHeader("Authorization");
-		final String authorizationHeader = "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbGllbiIsImV4cCI6MTc0NjM0NDExOSwiaWF0IjoxNzQ2MjU3NzE5fQ.L2Ms8EAx3FTpSH-oi3yu8wDD5qo1Orv7NszyYxt4OBY
-      
->>>>>>> 36b5ffad85cf33920b41c3ea012aeada7fcf5cf8
 		 String username = null;
 	     String jwt = null;
  
